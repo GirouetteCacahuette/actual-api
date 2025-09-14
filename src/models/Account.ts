c54a9b478607db1003c632898f4c4723ec0022ed
Martin Prieur <martin.prieur@outlook.fr>
2025-09-14 17:21:30 +0200

    WIP Put types in separate files

---
+++ src/models/Account.ts
@@ -0,0 +1,12 @@
+import * as z from 'zod';
+
+export const Account = z.object({
+	id: z.string(),
+	name: z.string(),
+	type: z.string(),
+	balance: z.number(),
+	offbudget: z.boolean(),
+	closed: z.boolean()
+});
+
+export type Account = z.infer<typeof Account>;
